3e648d5d33caa1baf0d34a434aa1cf7614a78ece
gasht1 <gebriegashaw7@gmail.com>
2023-02-18 16:58:15 +0400

    hello

--- my_app/pages/index.js
+++ my_app/pages/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
+
 import Header from '../components/Header'
-import About from '../components/About'
+import Home from '../components/Home'
+import Shop from '../components/Shop'
+
 
 const index = () => {
   return (
-    <div>
+    < >
      <Header/>
-     <About/>
-    </div>
+     <Home/>
+     <Shop/>
+     
+    </>
   )
 }
 
